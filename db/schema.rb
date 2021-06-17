8edde8e467cc1988e626f87c8859abbab7080a4b
Cory Tveten <corytveten@gmail.com>
2021-06-16 21:09:50 -0400

    add score to users db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_024327) do
+ActiveRecord::Schema.define(version: 2021_06_17_010822) do
 
   create_table "questions", force: :cascade do |t|
     t.string "category"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_06_14_024327) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "score"
   end
 
 end
