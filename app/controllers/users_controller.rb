f4ff94492e6136dfdb6e69b536f97d0bba0ac9bc
Cory Tveten <corytveten@gmail.com>
2021-06-18 13:41:40 -0400

    create method for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,23 @@ class UsersController < ApplicationController
             render json: { message: 'Username not found'}
         end
     end
+
+    # def new
+    #     user = User.new
+    # end
+
+    def create
+        user = User.create(user_params)
+        if (user.save)
+            render json: user
+        else
+            render :error
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:username, :password, :score)
+    end
+    
 end
