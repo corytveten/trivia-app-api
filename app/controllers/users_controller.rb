b26a390a95b118bb6ab88a3177f6a1feb4b12368
Cory Tveten <corytveten@gmail.com>
2021-06-17 23:18:20 -0400

    debug user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users, except: [:created_at, :updated_at]
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user, except: [:created_at, :updated_at]
+        else
+            render json: { message: 'Username not found'}
+        end
+    end
 end
