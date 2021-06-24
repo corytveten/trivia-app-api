1728c7811305535d04ac0715b1140c2f68f9b577
Cory Tveten <corytveten@gmail.com>
2021-06-24 13:59:19 -0400

    add session controller methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,26 @@
 class SessionsController < ApplicationController
 
-    def new
-        user = User.new
-    end
+    # def new
+    #     user = User.new
+    # end
 
     def create
-        session[:username] = params[:username]
+        user = User.find_by(:username => params[:session][:email])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render: json: {
+                error: 'Invalid credentials.'
+            }
+        end
     end
 
     def destroy
-        session.delete :username
+        session.clear
+        render json {
+            notics: 'You have logged out.'
+        }
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/' => 'users#index'
+
   resources :users
   resources :questions
 
