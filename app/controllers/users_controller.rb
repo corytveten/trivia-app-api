5e61ccee03ea21c10490d6e24ad1f1f5469eb852
Cory Tveten <corytveten@gmail.com>
2021-06-28 17:36:50 -0400

    comment cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
         user = User.new(user_params)
         if user.save
             session[:user_id] = user.id
-            render json: user
+            render json: user, except: [:created_at, :updated_at]
         else
             render :error
         end
